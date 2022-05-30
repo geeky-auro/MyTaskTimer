de44b1d96c2fb6f70ccec3b1736a19fd211ed3af
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2022-05-30 10:08:46 +0530

    Upgradation

--- app/src/main/java/com/aurosaswatraj/tasktimer/SettingsDialog.kt
+++ app/src/main/java/com/aurosaswatraj/tasktimer/SettingsDialog.kt
@@ -1,8 +1,14 @@
 package com.aurosaswatraj.tasktimer
 
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatDialogFragment
+import kotlinx.android.synthetic.main.settings_dialog.*
 import java.util.*
 
 
@@ -29,5 +35,116 @@ class SettingsDialog:AppCompatDialogFragment() {
         retainInstance = true
     }
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.settings_dialog, container, false)
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        Log.d(TAG, "onViewCreated: called")
+        super.onViewCreated(view, savedInstanceState)
+
+        dialog?.setTitle(R.string.action_settings)
+
+        okButton.setOnClickListener {
+            saveValues()
+            dismiss()
+        }
+
+        ignoreSeconds.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                if (progress < 12) {
+                    ignoreSecondsTitle.text = getString(R.string.settingsIgnoreSecondsTitle,
+                        deltas[progress],
+                        resources.getQuantityString(R.plurals.settingsLittleUnits, deltas[progress]))
+                } else {
+                    val minutes = deltas[progress] / 60
+                    ignoreSecondsTitle.text = getString(R.string.settingsIgnoreSecondsTitle,
+                        minutes,
+                        resources.getQuantityString(R.plurals.settingsBigUnits, minutes))
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                TODO("Not yet implemented")
+            }
+        })
+
+        cancelButton.setOnClickListener { dismiss()}
+        }
+
+
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+
+        Log.d(TAG, "onViewStateRestored: called")
+        super.onViewStateRestored(savedInstanceState)
+
+        if (savedInstanceState == null) {
+            readValues()
+
+            firstDaySpinner.setSelection(firstDay - GregorianCalendar.SUNDAY)  // spinner values are zero-based
+
+            // convert seconds into an index into the time values array
+            val seekBarValue = deltas.binarySearch(ignoreLessThan)
+            if (seekBarValue < 0) {
+                // this shouldn't happen, the programmer's made a mistake
+                throw IndexOutOfBoundsException("Value $seekBarValue not found in deltas array")
+            }
+            ignoreSeconds.max = deltas.size - 1
+            Log.d(TAG, "onViewStateRestored: setting slider to $seekBarValue")
+            ignoreSeconds.progress = seekBarValue
+
+            if (ignoreLessThan < 60) {
+                ignoreSecondsTitle.text = getString(R.string.settingsIgnoreSecondsTitle,
+                    ignoreLessThan,
+                    resources.getQuantityString(R.plurals.settingsLittleUnits, ignoreLessThan))
+            } else {
+                val minutes = ignoreLessThan / 60
+                ignoreSecondsTitle.text = getString(
+                    R.string.settingsIgnoreSecondsTitle,
+                    minutes,
+                    resources.getQuantityString(R.plurals.settingsBigUnits, minutes)
+                )
+            }
+    }
+
+
+    }
+
+    private fun readValues() {
+        with(PreferenceManager.getDefaultSharedPreferences(context)) {
+            firstDay = getInt(SETTINGS_FIRST_DAY_OF_WEEK, defaultFirstDayOfWeek)
+            ignoreLessThan = getInt(SETTINGS_IGNORE_LESS_THAN, SETTINGS_DEFAULT_IGNORE_LESS_THAN)
+        }
+        Log.d(TAG, "Retrieving first day = $firstDay, ignoreLessThan = $ignoreLessThan")
+    }
+
+    private fun saveValues() {
+        val newFirstDay = firstDaySpinner.selectedItemPosition + GregorianCalendar.SUNDAY
+        val newIgnoreLessThan = deltas[ignoreSeconds.progress]
+
+        Log.d(TAG, "Saving first day = $newFirstDay, ignore seconds = $newIgnoreLessThan")
+
+        with(PreferenceManager.getDefaultSharedPreferences(context).edit()) {
+            if (newFirstDay != firstDay) {
+                putInt(SETTINGS_FIRST_DAY_OF_WEEK, newFirstDay)
+            }
+            if (newIgnoreLessThan != ignoreLessThan) {
+                putInt(SETTINGS_IGNORE_LESS_THAN, newIgnoreLessThan)
+            }
+            apply()
+        }
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "onDestroy: called")
+        super.onDestroy()
+    }
 }
